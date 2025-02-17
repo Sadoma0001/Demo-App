14fecbf3b1a4d640cbe9c9eb581462e097296ec9
sadoma0001 <salhujairi0001@stu.edu.kau.sa>
2025-02-17 11:53:28 +0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Demo-App',
+  webDir: 'www'
+};
+
+export default config;
